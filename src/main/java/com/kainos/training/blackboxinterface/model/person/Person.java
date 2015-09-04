f67e74d66ffbefb510ea546e92a9cc451e622461
James Taylor <j.taylor@kainos.com>
2015-09-04 12:13:31 +0100

    Add required files

--- src/main/java/com/kainos/training/blackboxinterface/model/person/Person.java
+++ src/main/java/com/kainos/training/blackboxinterface/model/person/Person.java
@@ -1,22 +1,22 @@
 package com.kainos.training.blackboxinterface.model.person;
 
 public class Person {
-    private String name;
-    private int id;
+	private String name;
+	private int id;
 
-    public void setId(int id) {
-    	this.id = id;
-    }
-    
-    public int getId() {
-    	return id;
-    }
-    
-    public String getName(){
-	  return name;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setName(String name){
-	  this.name = name;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
